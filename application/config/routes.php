9741aa2f979b1e473bde47d6d661c7e9e62a9a2c
Calvinn097 <Calvinwangxz@gmail.com>
2016-11-16 23:23:16 +0700

    SETTING REST API

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,4 +53,5 @@ $route['default_controller'] = 'Main_home';
 $route['404_override'] = '';
 // $route['translate_uri_dashes'] = FALSE;
 $route['ADMIN/(.+)'] = "ADMIN/$1";
+$route['API/(.+)'] = "API/$1";
 $route['(.+)'] = "MAIN/$1";
