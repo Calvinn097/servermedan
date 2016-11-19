a786cc6d11dd0ac00a7c1737a929eec2734df71a
myevil <williamkarno@gmail.com>
2016-11-20 02:03:21 +0700

    Change home grayscale add image ass

--- application/views/MAIN/home.php
+++ application/views/MAIN/home.php
@@ -43,9 +43,26 @@
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2">
                 <h2>Kategori Jasa Servis dan Reparasi</h2>
-                <p>Grayscale is a free Bootstrap 3 theme created by Start Bootstrap. It can be yours right now, simply download the template on <a href="http://startbootstrap.com/template-overviews/grayscale/">the preview page</a>. The theme is open source, and you can use it for any purpose, personal or commercial.</p>
-                <p>This theme features stock photos by <a href="http://gratisography.com/">Gratisography</a> along with a custom Google Maps skin courtesy of <a href="http://snazzymaps.com/">Snazzy Maps</a>.</p>
-                <p>Grayscale includes full HTML, CSS, and custom JavaScript files along with LESS files for easy customization.</p>
+                <div class="row">
+                    <div class="col-md-4 onthespot">
+                        <img src="../../../asset/images/onthespot.jpg"/>
+                        <h3>On The Spot</h3>
+                        <p>Reparasi on the spot merupakan jenis reparasi yang dibutuhkan segera dan dapat langsung dipanggil sekarang juga untuk memperbaiki barang yang rusak</p>
+                        <button type="button" id="more" class="btn btn-danger">Read More</button>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="../../../asset/images/onthespot.jpg"/>
+                        <h3>On The Spot</h3>
+                        <p>Reparasi on the spot merupakan jenis reparasi yang dibutuhkan segera dan dapat langsung dipanggil sekarang juga untuk memperbaiki barang yang rusak</p>
+                        <button type="button" id="more" class="btn btn-danger">Read More</button>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="../../../asset/images/onthespot.jpg"/>
+                        <h3>On The Spot</h3>
+                        <p>Reparasi on the spot merupakan jenis reparasi yang dibutuhkan segera dan dapat langsung dipanggil sekarang juga untuk memperbaiki barang yang rusak</p>
+                        <button type="button" id="more" class="btn btn-danger">Read More</button>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
