660be0d6f455fead12a1d2706eeeb2a02f954a42
myevil <williamkarno@gmail.com>
2017-01-13 12:56:07 +0700

    forgot_password

--- application/views/MAIN/forgot_password.php
+++ application/views/MAIN/forgot_password.php
@@ -6,28 +6,40 @@
     <style>
         .forget{
             width: 350px;
-            margin-top: 200px;
+            height : 400px;
+            margin-top: 100px;
             background-color : lightblue;
             box-shadow : 3px 3px 5px 0px;
+            border-radius : 10px;
+            text-align : center;
+        }
+        .forget p{
+            margin : 30px auto;   
+        }
+        .forget input [type:email]{
+            width: 100%;   
+        }
+        .forget img{
+            margin-top:30px;   
         }
     </style>
 </head>
 
  <!-- margin untuk dorong ke bwh biar gk ketutup header gktw kenapa top saja -->
-    <div class="container forget">
-        <div class="panel-body">
-            <p>Kehilangan password</p>
-            <form action="<?=base_url("user/check_user_email")?>" method="post">
-            <div class="row">
-                <div class="col-md-2">
-                    <label>Email:</label>
-                </div>
-                <div class="col-md-10">
-                    <input type="email" name="email">
-                    <input type="submit" value="Simpan">
-                </div>
+<div class="container forget">
+    <img src="<?=base_url("asset/images/laptop.jpg")?>" width="100" height="100">
+    <p>
+        Silahkan masukkan email anda untuk melakukan verifikasi diri
+    </p>
+    <form action="<?=base_url("user/check_user_email")?>" method="post">
+        <div class="row">
+            <div class="col-md-3">
+                <label>Email:</label>
             </div>
-        </form>
+            <div class="col-md-9">
+                <input type="email" name="email">
+                <input type="submit" value="Submit">
+            </div>
         </div>
-        
-    </div>
+    </form>
+</div>
